6869eb50d36c7058bbd125df64d8121d2819a0b6
Zhandin Pavel <pavelzhandin@gmail.com>
2020-07-05 20:35:01 +0300

    complete login part add navigation to all pages start of making assignments.html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+
+
+function changleVisibility() {
+    $("#dimm").toggleClass("d-none");
+    $("#social_network").toggleClass("d-none");
+}
+
+function showPhoneBlock() {
+    $("#autorization_hr").toggleClass("d-none");
+    $("#phone_autorization_block").toggleClass("d-none");
+}
+
+function hidePhoneBlock() {
+    $("#autorization_hr").addClass("d-none");
+    $("#phone_autorization_block").addClass("d-none");
+}
+
+$("#dimm").on("click", changleVisibility);
+$("#dimm").on("click", hidePhoneBlock);
+$("#login").on("click", changleVisibility);
+
+
+$("#phone_autorization").on("click", showPhoneBlock);
+
